446cc0b66e7edd301cf4e07508ec54083f39065c
skl <skl@192.168.245.1>
2015-03-12 15:37:48 +0100

    exemple edition des params de connexion

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-echo phpinfo();
+include("sshConnectClass.php");
 
-?>
+
+echo "hello world<br>";
+
+
+$editConnexion = new ReflectionClass('NiceSSH');
+$connexion = new NiceSSH();
+
+$editHote = $editConnexion->getProperty('ssh_host');
+$editHote->setAccessible(true);
+
+$editHote->setValue($connexion, 'mon.serveur.com');
+var_dump($editHote->getValue($connexion));
+
+
+?> 
